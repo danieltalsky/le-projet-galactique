86510acea155fd041e93485d26b7687467c884ff
Daniel Talsky <danieltalsky@gmail.com>
2019-04-02 09:45:24 -0400

    Now there are some files that do things

--- js/all-the-magic-starts-here.js
+++ js/all-the-magic-starts-here.js
@@ -1,1 +1,26 @@
-gamefield = getElementById('gamefield');
+/* element server */
+let es = {
+    create() {
+        return;
+    },
+    makeDiv(elementId, className, textContent) {
+        let div = document.createElement('div');
+        div.id = elementId;
+        div.className = className;
+        div.textContent = textContent;
+        return div;
+    },
+}
+
+
+let leetleMenu = document.getElementById('leetleMenu');
+leetleMenu.prepend(es.makeDiv('leetleMenuBorder', 'border', 'cleek me eventually'))
+
+let viewport = document.getElementById('viewport');
+
+let readout = document.getElementById('readout');
+readout.textContent = 'Some Magic Has Happened!';
+
+let collection = document.getElementById('collection');
+
+
