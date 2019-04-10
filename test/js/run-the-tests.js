9eebc20734b23c412fd8810338a91d16fe274dd3
Daniel Talsky <danieltalsky@gmail.com>
2019-04-10 16:31:54 -0400

    Add passing tests

---
+++ test/js/run-the-tests.js
@@ -0,0 +1,9 @@
+import {
+  DOMStage,
+  ElementServer,
+  WorldState,
+} from '/js/all-your-library-are-belong.js';
+
+/* Very Low Testing Burden, Javascript just has to not error on initial state */
+const testOutput = document.getElementById('testOutput');
+testOutput.innerText = 'Tests Passed';
