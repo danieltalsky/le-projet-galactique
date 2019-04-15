589e9625cd5fe57a806b6fa2f801503cfb3c26a9
Daniel Talsky <danieltalsky@gmail.com>
2019-04-15 19:32:11 -0400

    Add more richness and fix bugs

---
+++ js/all-the-fake-sentience-is-here.js
@@ -0,0 +1,16 @@
+
+class LivingThing {
+
+}
+class SentientThing extends LivingThing {
+
+}
+class AlienCreature extends SentientThing {
+
+}
+
+export {
+  LivingThing,
+  SentientThing,
+  AlienCreature,
+};
